79b9f463e3255a8c4543614d0635af0d463fbe4f
Rodrigo Campos <rodrigo.ccm@live.com>
2020-12-08 17:43:52 -0300

    Function tests example

--- functions.test.js
+++ functions.test.js
@@ -7,3 +7,33 @@ test("Adds 2 + 2 to equal 4", () => {
 test("Adds 2 + 2 to not equal 5", () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
+
+test("Should be null", () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+test("Should be falsy", () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
+
+test("Should be Brad Traversy object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Brad",
+    lastName: "Traversy",
+  });
+});
+
+test("Should be UNDER 1600", () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600);
+});
+
+test("There is no I in team", () => {
+  expect("team").not.toMatch(/I/i);
+});
+
+test("Admin should be in usernames", () => {
+  usernames = ["john", "karen", "admin"];
+  expect(usernames).toContain("admin");
+});
