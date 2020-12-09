2823d5ab6e5980c73d8b4f66977337bd1ad3274a
Rodrigo Campos <rodrigo.ccm@live.com>
2020-12-09 11:21:11 -0300

    Installed Axios finished functions tests and started new tests

--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,5 @@
 const functions = require("./functions");
+const axios = require("axios");
 
 test("Adds 2 + 2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
@@ -36,4 +37,17 @@ test("There is no I in team", () => {
 test("Admin should be in usernames", () => {
   usernames = ["john", "karen", "admin"];
   expect(usernames).toContain("admin");
+});
+
+test("User fetched name should be Leanne Graham", () => {
+  expect.assertions(1);
+  return functions.fetchUser().then((data) => {
+    expect(data.name).toEqual("Leanne Graham");
+  });
+});
+
+test("User fetched name should be Leanne Graham", async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual("Leanne Graham");
 });
